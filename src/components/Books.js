488a354b1c1411b917db6eeeeebc50ab8aa47e8f
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-03-01 02:59:27 +0200

    initial setup

---
+++ src/components/Books.js
@@ -0,0 +1,12 @@
+import './books.css';
+
+import React from 'react';
+import Booklist from './BookList';
+
+const Books = () => (
+  <div className="books-container">
+    <Booklist />
+  </div>
+);
+
+export default Books;
---
+++ src/components/Categories.js
@@ -0,0 +1,11 @@
+import './categories.css';
+
+import React from 'react';
+
+const Categories = () => (
+  <div className="cat-container">
+    <h2>Under construction</h2>
+  </div>
+);
+
+export default Categories;
